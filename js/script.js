9f3893ddb10970b92186d298696773d65e2efc09
ilyesbensid <benilyes666@gmail.com>
2026-07-10 16:42:21 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,41 +2,50 @@ const imageInput = document.getElementById("imageInput");
 const preview = document.getElementById("preview");
 const result = document.getElementById("result");
 
+
+// اختيار الصورة وعرضها
 imageInput.addEventListener("change", () => {
 
     const file = imageInput.files[0];
 
-    if(file){
+    if (file) {
         preview.src = URL.createObjectURL(file);
         preview.style.display = "block";
-        document.getElementById("uploadText").innerHTML="✅ تم اختيار الصورة";
+
+        document.getElementById("uploadText").innerHTML =
+            "✅ تم اختيار الصورة";
     }
 
 });
 
-document.getElementById("analyzeBtn").addEventListener("click", async ()=>{
 
-    result.innerHTML="⌛ جاري الاتصال بالخادم...";
+// زر تحليل الوجبة
+document.getElementById("analyzeBtn").addEventListener("click", async () => {
 
-    try{
+    result.innerHTML = "⌛ جاري الاتصال بالخادم...";
 
-        const response = await fetch("http://localhost:8000");
+    try {
+
+        const response = await fetch(
+            "https://nutrilens-production-4794.up.railway.app"
+        );
 
         const data = await response.json();
 
-        result.innerHTML=`
-        <h3>✅ تم الاتصال بالخادم</h3>
+        result.innerHTML = `
+            <h3>✅ تم الاتصال بالخادم</h3>
 
-        <p><b>المشروع:</b> ${data.project}</p>
+            <p><b>المشروع:</b> ${data.project}</p>
 
-        <p><b>الحالة:</b> ${data.status}</p>
+            <p><b>الحالة:</b> ${data.status}</p>
 
-        <p>${data.message}</p>
+            <p>${data.message}</p>
         `;
 
-    }catch(error){
+    } catch (error) {
 
-        result.innerHTML="❌ لم أستطع الاتصال بالخادم";
+        result.innerHTML =
+            "❌ لم أستطع الاتصال بالخادم";
 
     }
 
