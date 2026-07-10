e48f088e3e6bf89af19b85fa806165dfd4635db8
ilyesbensid <benilyes666@gmail.com>
2026-07-10 18:30:40 +0100

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -1,18 +1,10 @@
 from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse
-import os
 
 app = FastAPI(
     title="NutriLens API",
     version="0.1.0"
 )
-
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
-app.mount("/css", StaticFiles(directory=os.path.join(BASE_DIR, "css")), name="css")
-app.mount("/js", StaticFiles(directory=os.path.join(BASE_DIR, "js")), name="js")
 
 app.add_middleware(
     CORSMiddleware,
@@ -24,11 +16,11 @@ app.add_middleware(
 
 @app.get("/")
 def home():
-    return FileResponse(os.path.join(BASE_DIR, "index.html"))
-
-@app.get("/login.html")
-def login():
-    return FileResponse(os.path.join(BASE_DIR, "login.html"))
+    return {
+        "project": "NutriLens",
+        "status": "online",
+        "message": "🚀 FastAPI يعمل بنجاح"
+    }
 
 @app.post("/analyze")
 async def analyze_food(image: UploadFile = File(...)):
